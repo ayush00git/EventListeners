a877ba62ad3944fd9f2c7fa75bcd8f883978c223
ayush00git <ayush.2007.iit@gmail.com>
2025-05-15 16:02:42 +0530

    Syntax of EL and event = 'click'

--- script.js
+++ script.js
@@ -4,5 +4,22 @@ console.log(view1)
 view1.style.display = "none"
 
 const view = document.querySelector("#view2")
+view.style.display = "flex"
 const div = view.querySelector("div")
 const h2 = div.querySelector("h2")
+
+// syntax -- addEventListener(event, function, useCapture)
+doSomething = () => {
+    alert("Doing something..")
+}
+
+h2.addEventListener("click", doSomething, false)
+// flase is by deaulft do you can even ignore writing it
+// how to remove the EventListener
+h2.removeEventListener("click", doSomething, false)
+
+// using an anonymous function (but remember that removeEventListener function does not works with anonymous fxns)
+h2.addEventListener("click", function (textChange) {
+    console.log(textChange.target)                         // .target returns h2
+    textChange.target.textContent = "Clicked"
+})
