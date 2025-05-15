e44f82d56d8d94f92711e51cf580d86e99dcbae3
ayush00git <ayush.2007.iit@gmail.com>
2025-05-15 15:39:53 +0530

    Step1. Access the elements using DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+// First access all the elements
+const view1 = document.querySelector("#view1")
+console.log(view1)
+view1.style.display = "none"
+
+const view = document.querySelector("#view2")
+const div = view.querySelector("div")
+const h2 = div.querySelector("h2")
