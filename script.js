2b0890e87735e23bc883e4c8364365f87c7cfdae
ayush00git <ayush.2007.iit@gmail.com>
2025-05-15 17:03:36 +0530

    Learned about readystatechange and event-bubbling

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-// First access all the elements
 const view1 = document.querySelector("#view1")
-console.log(view1)
 view1.style.display = "none"
 
-const view = document.querySelector("#view2")
-view.style.display = "flex"
-const div = view.querySelector("div")
-const h2 = div.querySelector("h2")
+const view2 = document.querySelector("#view2")
+view2.style.display = "flex"
 
-// syntax -- addEventListener(event, function, useCapture)
-doSomething = () => {
-    alert("Doing something..")
+document.addEventListener("readystatechange", (event) => {
+    if(event.target.readyState === "complete"){                                     // event.target refers to document
+        console.log("readystate : completed")
+        initApp()                           
+    }
+})
+
+const initApp = () => {
+    const view2 = document.querySelector("#view2")
+    const div = view2.querySelector("div")
+    const h2 = div.querySelector("h2")
+
+    view2.addEventListener("click", (event) => {
+        event.target.style.backgroundColor = "purple"           // view2.style.backgroundColour will also work
+    })
+
+    div.addEventListener("click", (event) => {
+        event.stopPropagation()
+        event.target.style.backgroundColor = "pink"
+    })
+
+    h2.addEventListener("click", (event) => {
+        event.stopPropagation()
+        event.target.textContent = "Clicked"
+    })
 }
 
-h2.addEventListener("click", doSomething, false)
-// flase is by deaulft do you can even ignore writing it
-// how to remove the EventListener
-h2.removeEventListener("click", doSomething, false)
+// Event bubbling is seen in these cases, like as we'll click the h2 tag the view and div EventListener will wake up 
+// as the click is also being inside them, so the event gets propagated
 
-// using an anonymous function (but remember that removeEventListener function does not works with anonymous fxns)
-h2.addEventListener("click", function (textChange) {
-    console.log(textChange.target)                         // .target returns h2
-    textChange.target.textContent = "Clicked"
-})
+// stopPropagation() method is used to stop this event bubbling
